39b0bff0c84ce6e1a9944c2c4b5579c88fa3801c
sunidhijaiswal <33841332+sunidhijaiswal@users.noreply.github.com>
2019-07-04 19:01:51 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,26 @@
+import csv
+
+teams = []
+total_played = []
+winner = []
+x = {}
+
+# Read csv file matches.csv
+
+with open('matches.csv', 'r') as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+        teams.append(row[4])
+        total_played.append(row[4])
+        total_played.append(row[5])
+        winner.append(row[10])
+
+file.close()
+#filter common names teams
+teams = set(teams)
+#convert into dict
+for i in teams:
+    x[i] = {"Total_played": total_played.count(i), "won": winner.count(i), "lost": total_played.count(i) - winner.count(i)}
+
+print(x)
